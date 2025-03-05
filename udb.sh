bd6316579a26d15fdf87a067c9e2d3f9c87e38d2
mileticb <budimir.miletic@prodyna.com>
2025-03-05 15:11:51 +0100

    update devbox to use zsh

--- udb.sh
+++ udb.sh
@@ -2,4 +2,9 @@
 
 curl -o devbox.json https://raw.githubusercontent.com/budam/setup-devops/main/devbox.json
 
+echo "devbox.json downloaded"
+
+curl -o ~/.zshrc https://raw.githubusercontent.com/budam/setup-devops/main/zshrc
+
+echo "zshrc downloaded"
 echo "Finished!"
